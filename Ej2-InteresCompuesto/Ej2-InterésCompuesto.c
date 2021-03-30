c65b00bb7683f55abca1b2dc27a2ee452c287b97
Tobias Banno <tobiasbanno@gmail.com>
2021-03-30 15:25:35 -0300

    Terminado

--- Ej2-InteresCompuesto/Ej2-InterésCompuesto.c
+++ Ej2-InteresCompuesto/Ej2-InterésCompuesto.c
@@ -13,11 +13,11 @@ int main()
 	printf("El monto con el cual se hace el calculo es de $1000\n");
 	printf("---------------------------------------------------------------------------\n");
 	printf("Seleccione:\n");
-	printf("a- tasas de inter�s del 5 por ciento\n");
-	printf("b- tasas de inter�s del 6 por ciento\n");
-	printf("c- tasas de inter�s del 8 por ciento\n");
-	printf("d- tasas de inter�s del 9 por ciento\n");
-	printf("e- tasas de inter�s del 10 por ciento\n");
+	printf("a- tasas de interes del 5 por ciento\n");
+	printf("b- tasas de interes del 6 por ciento\n");
+	printf("c- tasas de interes del 8 por ciento\n");
+	printf("d- tasas de interes del 9 por ciento\n");
+	printf("e- tasas de interes del 10 por ciento\n");
 	scanf("%c",&opcion);
 	
 	switch(opcion){
@@ -37,7 +37,7 @@ int main()
 				break;
 	}
 	printf("---------------------------------------------------------------------------\n");
-	printf("Seleccionaste la opci�n %c \n",opcion);
+	printf("Seleccionaste la opcion %c \n",opcion);
 	printf("-------------------------- \n");
 	printf("%4s %21s\n","anio","Monto del deposito");
 	for (anio= 1; anio <= 10; anio++)
