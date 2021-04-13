669c4c4713c9667c40a6b9ab93d03790993eac39
Tobias Banno <tobiasbanno@gmail.com>
2021-04-13 19:05:26 -0300

    Prueba

---
+++ Ej6-ADC-PWM/src/Ej6-ADC-PWM.cpp
@@ -0,0 +1,20 @@
+#include <Arduino.h>
+
+#define pote A0
+#define led 4
+
+int voltajePote=0;
+void setup() 
+{
+  pinMode(pote, INPUT);
+  pinMode(led, OUTPUT);
+
+}
+
+void loop() {
+
+  voltajePote=analogRead(pote);
+
+  analogWrite(led,voltajePote);
+
+}
