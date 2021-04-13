9b99519bf8bec9e4c24ac157ea6ee352b5c7bfdc
Tobias Banno <tobiasbanno@gmail.com>
2021-04-13 13:48:22 -0300

    Terminado

---
+++ Ej5-SecuenciaLed/Ej5-SecuenciaLed/Ej5-SecuenciaLed.ino
@@ -0,0 +1,53 @@
+#define pulsador_A 11
+#define pulsador_B 12
+
+#define Led_1 4
+#define Led_2 5
+#define Led_3 6
+void setup() {
+
+pinMode(pulsador_A, INPUT);
+pinMode(pulsador_B, INPUT);
+
+pinMode(Led_1, OUTPUT);
+pinMode(Led_2, OUTPUT);
+pinMode(Led_3, OUTPUT);
+
+
+}
+
+void loop() 
+{
+  if(digitalRead (pulsador_A)==1&&digitalRead(pulsador_B)==0)
+  {
+   digitalWrite(Led_1,HIGH);
+   delay(1000);
+   digitalWrite(Led_1,LOW);
+   
+   digitalWrite(Led_2,HIGH);
+   delay(1000);
+   digitalWrite(Led_2,LOW);
+   
+   digitalWrite(Led_3,HIGH);
+   delay(1000);
+   digitalWrite(Led_3,LOW);
+  }
+  else
+  {
+    if(digitalRead (pulsador_A)==0&&digitalRead(pulsador_A)==1)
+    {
+    digitalWrite(Led_3,HIGH);
+    delay(1000);
+    digitalWrite(Led_3,LOW);
+    
+    digitalWrite(Led_2,HIGH);
+    delay(1000);
+    digitalWrite(Led_2,LOW);
+    
+    digitalWrite(Led_1,HIGH);
+     delay(1000);
+    digitalWrite(Led_1,LOW);
+    }
+  }
+  
+}
