e768e5535dc90ae2fed49326c2be2eee4fc46179
Tobias Banno <tobiasbanno@gmail.com>
2021-05-21 15:07:59 -0300

    Cambio

--- Ej10-Display7Segmentos/src/main.cpp
+++ Ej10-Display7Segmentos/src/main.cpp
@@ -1,14 +1,15 @@
 #include <Arduino.h>
 
 #define TIEMPO 1500
+int ledEncendido;
 
 void setup() {
-  DDRD = DDRD | B11111111;          //Configura los pines del 0 al 7 como salida
+  DDRD = DDRD | B11111110;          //Configura los pines del 0 al 7 como salida
   
 }
 
 void loop() {
- for (ledEncendido = B00000001; ledEncendido <= B10000000; ledEncendido = ledEncendido<<1)
+ for (ledEncendido = B00000010; ledEncendido <= B10000000; ledEncendido = ledEncendido<<1)
   { 
     PORTD=ledEncendido;
     delay(TIEMPO);
