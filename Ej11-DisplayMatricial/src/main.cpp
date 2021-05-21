834ef5e7bbbcaa2654fd69b048fbf566e7ca5c05
Tobias Banno <tobiasbanno@gmail.com>
2021-05-21 16:08:39 -0300

    Subida 11

---
+++ Ej11-DisplayMatricial/src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+
+#define TIEMPO 1500
+int filaAlto, columnaAlto;
+
+void setup() {
+  DDRD = DDRD|B00001111;      // de pin 0 a pin 3 como salida   
+  DDRB = DDRB|B001111;      // de pin 8 a pin 11 como salida
+  
+}
+
+void loop() {
+for (columnaAlto = B00000001;columnaAlto <= B00001000; columnaAlto = columnaAlto<<1)
+  {
+    PORTB=columnaAlto; 
+    for(filaAlto = B0000001;filaAlto <= B00001000; filaAlto = filaAlto<<1) 
+    {
+      PORTD=~filaAlto;
+      delay(TIEMPO);
+    }
+  }
