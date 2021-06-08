19bf824b7e187cace92c4387c4306e89610136c1
Tobias Banno <tobiasbanno@gmail.com>
2021-06-08 15:37:50 -0300

    Cambios

--- Ej12-ArchivoDatos/Ej12-ArchivoDatosAlmacenar.c
+++ Ej12-ArchivoDatos/Ej12-ArchivoDatosAlmacenar.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
+#include <stdint.h>
 
 struct sensores
 {
-        char tipoSensor;
-        int valorSensor;
-        unsigned long tiempo;
+        uint32_t tiempo;           //Tiempo transcurrido
+        uint16_t valorSensor;           //Valor de sensor
+        uint8_t tipoSensor;            //Tipo de sensor leido
 }sensor1={'H',10,5},*ptrsensor;                //le damos valores cualquiera a la estructura, esto lo podriamos tmb hacer con un scanf o con un sensor
 
 int main(void)
 {
     ptrsensor=&sensor1;
     FILE *archivo;                       //declaración puntero archivo
-    archivo=fopen("archivo.bin","w");    // abrimos/creamos un archivo.bin
+    archivo=fopen("archivo.dat","w");    // abrimos/creamos un archivo.bin
 
     if(archivo == NULL) {
     printf("Error al abrir archivo!");
     return -1;
     }
 
-    fwrite(ptrsensor, sizeof(sensor1), 1, archivo);        //guardamos la estructura en el archivo.bin
-
-   // fprintf(archivo, 'struct sensores', sensor1);      // ESTO ES USADO PARA ARCHIVOS.TXT !!!! 
-
+    fwrite(ptrsensor, sizeof(struct sensores), 1, archivo);        //guardamos la estructura en el archivo.bin
+    // fprintf(archivo, 'struct sensores', sensor1);      // ESTO ES USADO PARA ARCHIVOS.TXT !!!! 
     fclose(archivo);                                    //cerramos el archivo escrito
+
+    return 0;
+
 }
--- Ej12-ArchivoDatos/Ej12-ArchivoDatosLeer.c
+++ Ej12-ArchivoDatos/Ej12-ArchivoDatosLeer.c
@@ -1,28 +1,29 @@
 #include <stdio.h>
-
+#include <stdint.h>
 struct sensores
 {
-        char tipoSensor;
-        int valorSensor;
-        unsigned long tiempo;
-}sensor1={'H',10,5},*ptrsensor;                //le damos valores cualquiera a la estructura, esto lo podriamos tmb hacer con un scanf o con un sensor
+        uint32_t tiempo;           //Tiempo transcurrido
+        uint16_t valorSensor;           //Valor de sensor
+         uint8_t tipoSensor;            //Tipo de sensor leido
+}sensor1,*ptrsensor;                //le damos valores cualquiera a la estructura, esto lo podriamos tmb hacer con un scanf o con un sensor
 
 int main(void)
 {
     ptrsensor=&sensor1;
     FILE *archivo;                       //declaración puntero archivo
-    archivo=fopen("archivo.bin","r");    // leemos un archivo.bin
+    archivo=fopen("archivo.dat","r");    // leemos un archivo.bin
 
     if(archivo == NULL) {
     printf("Error al abrir archivo!");
     return -1;
     }
 
-    fread(ptrsensor, sizeof(sensor1), 1, archivo);        //guardamos lo leido en nuestra estructura ya creada
+    fread(ptrsensor, sizeof(struct sensores), 1, archivo);        //guardamos lo leido en nuestra estructura ya creada
 
    // fscanf(archivo, 'struct sensores', sensor1);      // ESTO ES USADO PARA ARCHIVOS.TXT !!!! 
 
     fclose(archivo);                                    //cerramos el archivo escrito
 
-    printf("NOMBRE DL SENSOR: %s, VALOR DE SENSOR: %d, TIEMPO DEL SENSOR: %d \n", sensor1.tipoSensor,sensor1.valorSensor,sensor1.tiempo);
+    printf("NOMBRE DL SENSOR: %c, VALOR DE SENSOR: %d, TIEMPO DEL SENSOR: %d \n", sensor1.tipoSensor,sensor1.valorSensor,sensor1.tiempo);
+    return 0;
 }
